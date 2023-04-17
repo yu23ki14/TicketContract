dc5155c31a9bf906d3f665d8014f5dab4352fc40
yu23ki14 <yuki_021423@yahoo.co.jp>
2023-04-17 19:20:40 +0900

    change error message

--- contracts/community/InteractHenkakuToken.sol
+++ contracts/community/InteractHenkakuToken.sol
@@ -15,7 +15,7 @@ abstract contract InteractHenakuToken is Administration, MintManager {
     function transferHenkakuV2(uint256 _amount, address _to) internal {
         _checkHenkakuV2Balance(_amount);
         bool sent = IHenkakuToken(henkakuV2).transferFrom(msg.sender, _to, _amount);
-        require(sent, "Ticket: Henkaku transfer failed");
+        require(sent, "Ticket: ERC20 token transfer failed");
     }
 
     function batchTransferHenkakuV2(uint256 totalPrice, uint256[] memory _amounts, address[] memory _to) internal {
@@ -27,7 +27,7 @@ abstract contract InteractHenakuToken is Administration, MintManager {
 
         for (uint256 i = 0; i < amountsLength; ) {
             bool sent = IHenkakuToken(henkakuV2).transferFrom(msg.sender, _to[i], amounts[i]);
-            require(sent, "Ticket: Henkaku transfer failed");
+            require(sent, "Ticket: ERC20 Token transfer failed");
 
             unchecked {
                 ++i;
@@ -36,9 +36,6 @@ abstract contract InteractHenakuToken is Administration, MintManager {
     }
 
     function _checkHenkakuV2Balance(uint256 _requiredAmount) internal view {
-        require(
-            IHenkakuToken(henkakuV2).balanceOf(msg.sender) >= _requiredAmount,
-            "Ticket: Insufficient HenkakuV2 token"
-        );
+        require(IHenkakuToken(henkakuV2).balanceOf(msg.sender) >= _requiredAmount, "Ticket: Insufficient ERC20 token");
     }
 }
